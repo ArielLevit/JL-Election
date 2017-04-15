3ba899e4c2237ac9279bab066c9cd92037ba6599
Eitan <eitan.goldfrad@gmail.com>
2017-04-15 21:34:00 +0300

    fixed public folder

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var http = require('http');
-
+var path = require('path')
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res) {
     res.sendfile('views/index.html');
