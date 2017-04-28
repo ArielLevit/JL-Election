a0aab8cc712e08ba33ed343fd8f21f93a5c8fc44
ArielLevit <ariellevit2@gmail.com>
2017-04-28 15:38:01 +0300

    with youtube api

--- public/js/main.js
+++ public/js/main.js
@@ -10,9 +10,102 @@ $(document).ready(function() {
     })
 
 });
+//Show model and X button
+$(function candidates () {
+    $(".nav-tabs li a").on("click", function () {
+        var img = $(this).attr('data-img');
+        $('#' + img).attr('src','./img/models/' + img + '.png').delay(500).slideDown('slow');
+        $('.xButton').delay(800).slideDown('slow');
+    });
 
-function vote() {
-    var name = document.getElementById('vote');
-    alert("You have voted for *I WISH I KNEW JAVASCRIPT*");
+    $(".xButton").on("click", function b () {
+        var img = $(this).attr('data-img');
+        $(img).hide();
+        $(this).hide();
+    });
+});
+//Show model on load window at first time
+$( window ).on( "load", function () {
+    var imgX = $(".nav-tabs li a").attr('data-img');
+    $('#' + imgX).attr('src','./img/models/' + imgX + '.png').delay(500).slideDown('slow');
+    $('.xButton').delay(800).slideDown('slow');
+} );
+
+//Voting function
+var name;
+function voteMe(name) {
+    alert("Thanks for voting to " + name + ", see'ya in 4 years");
+}
+$(function () {
+    $('.button').on('click', function () {
+        var voteid = $(this).attr('data-voteid');
+        $(voteid).html(function(i, val) { return val*1+1 });
+    });
+});
+
+
+
+
+// var vC = 0;
+// var vA = 0;
+// var vB = 0;
+// var vWW = 0;
+// var vF = 0;
+// var vBO = 0;
+// var vC, vA, vB, vWW, vF, vBO;
+
+
+var heroId;
+function votesCounter(heroId, hero) {
+    hero++;
+
+    document.getElementById(heroId).innerHTML = hero.toString();
+    // switch (hero){
+    //     case "Cyborg":
+    //         vC += 1;
+    //         document.getElementById("vC").innerHTML = vC;
+    //         break;
+    //     case "Aquaman":
+    //         vA += 1;
+    //         document.getElementById("vA").innerHTML = vA;
+    //         break;
+    //     case "Batman":
+    //         vB += 1;
+    //         document.getElementById("vB").innerHTML = vB;
+    //         break;
+    //     case "Wonder Woman":
+    //         vWW += 1;
+    //         document.getElementById("vWW").innerHTML = vWW;
+    //         break;
+    //     case "The Flash":
+    //         vF += 1;
+    //         document.getElementById("vF").innerHTML = vF;
+    //         break;
+    //     case "Black Obama":
+    //         vBO += 1;
+    //         document.getElementById("vBO").innerHTML = vBO;
+    //         break;
+    // }
 }
 
+//Side Nav
+$(function() {
+    var navBtn = $('.nav-btn');
+    function toggleNav() {
+        navBtn.toggleClass('open');
+        $('nav').toggleClass('open');
+        $('.container').toggleClass('open');
+    }
+    navBtn.click(function() {
+        toggleNav();
+    });
+    $('nav ul li a').click(function(e) {
+        toggleNav();
+        return false;
+    })
+} );
+
+if (!window.jQuery)
+{
+    document.write('<script src="./js/jquery.1.11.min.js"><\/script>');
+}
---
+++ public/js/search.js
@@ -0,0 +1,35 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyCI9oAUZdUMiQLeFRvBsPOWVpCvf9rSiFE');
+}
+
+// Called when the search button is clicked in the html code
+function search() {
+    var query = document.getElementById('query').value;
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q:query
+    });
+    // Send the request to the API server, call the onSearchResponse function when the data is returned
+    request.execute(onSearchResponse);
+}
+// Triggered by this line: request.execute(onSearchResponse);
+function onSearchResponse(response) {
+    var results = response.result;
+    $("#results").html("");
+    $.each(results.items, function(index, item) {
+        $.get("./item.html", function(data) {
+            $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+        });
+    });
+
+}
